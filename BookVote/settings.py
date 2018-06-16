cf83a017074f54e5ee56f9ce6a1be03b81068aac
seanburnham <sburnham92@marriottschool.byu.edu>
2018-06-16 13:19:23 -0600

    updated settings

--- BookVote/settings.py
+++ BookVote/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import shutil
-from decouple import config
+
+# from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'et@hw1sv32tka@!_k0g=8)@x)y0ifk3j(vro=ce5w+zu@-lxv^'
+SECRET_KEY = 'et@hw1sv32tka@!_k0g=8)@x)y0ifk3j(vro=ce5w+zu@-lxv^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -87,12 +88,12 @@ WSGI_APPLICATION = 'BookVote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default='postgres://ctzjohvwiaeivx:071a1544d51e9e01575600087dc571fbf7f7f0fa56a19eedb3bee62eff36d1a0@ec2-54-83-59-120.compute-1.amazonaws.com:5432/d6rmlbc2q1qbui'
     )
 }
 
