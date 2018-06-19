e4b50b0abe8104df373667775782d72ea4fa473a
seanburnham <sburnham92@marriottschool.byu.edu>
2018-06-19 12:14:09 -0600

    Trying to add urls

--- BookVote/urls.py
+++ BookVote/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     # the built-in Django administrator
     url(r'^admin/', admin.site.urls),
+    url(r'^books/', include('books.urls')),
 
     # urls for any third-party apps go here
 
