c03e3dbd775d40078ae0cdba84d4235a4e564ccd
seanburnham <sburnham92@marriottschool.byu.edu>
2018-07-03 22:48:18 -0600

    deleted test page

--- accounts/views/test.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
- 
-from django.conf import settings
-from django_mako_plus import view_function, jscontext
-from datetime import datetime, timezone
-
-@view_function
-def process_request(request):
-	return render(request, 'test.html')
