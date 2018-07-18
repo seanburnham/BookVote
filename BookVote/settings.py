d0f4540f17a9b3438675ba3494c86403682fc2b6
seanburnham <sburnham92@marriottschool.byu.edu>
2018-07-18 00:11:26 -0600

    Updated settings

--- BookVote/settings.py
+++ BookVote/settings.py
@@ -149,25 +149,25 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Database
 SECRET_KEY = config('SECRET_KEY')
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#         )
-# }
-
-# DEBUG = config('DEBUG', default=False, cast=bool)
-
-DEBUG = True
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'SeanBurnham',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+        )
 }
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+# DEBUG = True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'SeanBurnham',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
